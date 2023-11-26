0410b6a12260c38c6809c85bc7fc0565de5163be
lukethomas1 <lukethethomas@gmail.com>
2023-11-25 18:55:09 -0800

    Fix math; Add more comments; Add .gitignore

--- constants.py
+++ constants.py
@@ -13,17 +13,11 @@ CHANCE_TO_DROP_WITH_TWO_LINES = 0.09
 CHANCE_TO_DROP_WITH_THREE_LINES = 0.01
 CHANCE_TO_UPGRADE_POT = 0.05
 CHANCE_TO_INCREASE_LINES = 0.05
+
 """
 First line is always prime, second and third have 90% chance to be lower
 """
 CHANCE_OF_PRIME_LINE = 0.1
-"""
-Certain potentials only show on specific equips.
-For example, only weapons can get Watk%, damage to boss%, total damage%, etc.
-Gloves can get Decent Sharp Eyes, etc. (Can only get these lines 1 time per item)
-
-Disabled potentials: Watk% on Kataras. Matk% on weapons.
-"""
 
 ALL_POSSIBLITIES = [
     "% LUK",
@@ -39,7 +33,14 @@ ALL_POSSIBLITIES = [
     "% MAGIC DEF",
 ]
 
-POTENTIAL_POSSIBILITIES = {
+"""
+Certain potentials only show on specific equips.
+For example, only weapons can get Watk%, damage to boss%, total damage%, etc.
+Gloves can get Decent Sharp Eyes, etc. (Can only get these lines 1 time per item)
+
+Disabled potentials: Watk% on Kataras. Matk% on weapons.
+"""
+EQUIP_POSSIBILITIES = {
     "HAT": [
 
     ],
@@ -86,6 +87,7 @@ POTENTIAL_POSSIBILITIES = {
 }
 
 # Prime line is first value, non-prime second value
+# assuming lvl 200+ item unique potential
 UNIQUE_LINE_VALUES = {
     "% LUK": [15, 10],
     "% DEX": [15, 10],
--- maple-cube-sim.py
+++ maple-cube-sim.py
@@ -4,49 +4,77 @@ import argparse
 
 import constants
 
-def run_sim(stat_threshold, equip_type, run_amount):
 
-    stat_hits = {}
+def run_sim(stat_threshold: int, equip_type: str, cube_amount: int):
+    """Simulate X cubes on a given equip type, printing out stat hits above a given threshold
 
-    for cube_index in range(run_amount):
+    Args:
+        stat_threshold (int): % stat threshold to print out at the end, ex. 30
+        equip_type (str): Equip type to simulate, ex. SHOES (see keywords in constants.py)
+        cube_amount (int): # of cubes to use, e.g 10000
+    """
+
+    # Pre-fill the dictionary to reduce time it takes for long runs
+    stat_hits = {stat: {} for stat in constants.UNIQUE_LINE_VALUES.keys()}
+
+    # Multiples of five between stat_threshold and 45 (including 45)
+    for key in stat_hits.keys():
+        for percent_val in range(stat_threshold, 46, 5):
+            stat_hits[key][percent_val] = 0
+
+    # Do simulation
+    for _ in range(cube_amount):
         lines = simulate_random_three_lines(equip_type)
         values = simulate_stat_values(lines)
         stats = count_stats_from_lines(lines, values)
 
         for stat, value in stats.items():
             if value >= stat_threshold:
-                if stat not in stat_hits:
-                    stat_hits[stat] = {}
-                if value not in stat_hits[stat]:
-                    stat_hits[stat][value] = 1
-                else:
-                    stat_hits[stat][value] += 1
+                stat_hits[stat][value] += 1
 
-    print("-----STAT HITS-----")
+    # Print results
+    print("---------------STAT HITS---------------")
+    print(f"Equip Type: f{equip_type}")
+    print(f"Total cubes used: {cube_amount:,}\n")
     for stat_name, percent_dict in stat_hits.items():
         print(f"{stat_name}:")
         for percent, count in sorted(percent_dict.items()):
-            print(f"    {percent}%: {count} hits, {count/run_amount:.3%}")
-    print(f"Total cubes used: {run_amount}")
+            if count > 0:
+                print(f"    {percent}%: {count} hits, {count/cube_amount:.5%}, 1 out of ~{cube_amount/count:,.0f} cubes")
+
 
 def count_stats_from_lines(three_lines: (str,str,str), three_values: (int,int,int)) -> dict[str, int]:
+    """Count stat totals of the given 3 lines & 3 values, return a dict of each stat with the % given by the potential
+
+    Args:
+        three_lines (str,str,str): The potential line description, ex. '% LUK'
+        three_values (int,int,int): The value of each potential line, ex. '15'
+
+    Returns:
+        dict[str, int]: Dict of all five (incl. All Stat) stats to % stat given by potential, e.x '% LUK': 15
+    """
     stat_counts = {key: 0 for key in constants.UNIQUE_LINE_VALUES.keys()}
 
     for i in range(len(three_lines)):
         if three_lines[i] == "% ALL STAT":
+            # Add value of all stat line to every stat
             stat_counts = {stat: value + three_values[i] for stat, value in stat_counts.items()}
         elif three_lines[i] in stat_counts:
             stat_counts[three_lines[i]] += three_values[i]
 
     return stat_counts
 
+
 def simulate_random_three_lines(equip_type: str) -> (str,str,str):
-    """asdf
+    """Generate 3 random potential line descriptions
 
     Args:
-        equip_type (str): ex. "SHOES"
+        equip_type (str): Equip type to generate lines for, ex. "SHOES"
+
+    Returns:
+        (str,str,str): Tuple with 3 string values representing the line description, ex. ('% LUK', "% HP", "% AVOID")
     """
-    possibilities = constants.ALL_POSSIBLITIES + constants.POTENTIAL_POSSIBILITIES[equip_type]
+    possibilities = constants.ALL_POSSIBLITIES + constants.EQUIP_POSSIBILITIES[equip_type]
 
     three_lines = []
 
@@ -55,21 +83,43 @@ def simulate_random_three_lines(equip_type: str) -> (str,str,str):
         three_lines.append(line)
 
         # Can only get one instance of an equip-specific line
-        if line in constants.POTENTIAL_POSSIBILITIES[equip_type]:
+        if line in constants.EQUIP_POSSIBILITIES[equip_type]:
             possibilities.remove(line)
     return (*three_lines,)
 
+
 def simulate_stat_values(three_lines: (str,str,str)) -> (int,int,int):
+    """Generate values for the given potential lines. Use 0 as value for non-helpful lines.
+
+    Args:
+        three_lines (str,str,str): The three lines to generate values for, ex. ("% LUK", "% HP", "% AVOID")
+
+    Returns:
+        (int,int,int): Tuple with 3 int values corresponding to the given stat
+    """
     three_values = []
 
-    for line in three_lines:
+    for i in range(len(three_lines)):
+        line = three_lines[i]
+
+        # If line is a stat line
         if line in constants.UNIQUE_LINE_VALUES:
-            value = random.choices(constants.UNIQUE_LINE_VALUES[line], weights=[1 - constants.CHANCE_OF_PRIME_LINE, constants.CHANCE_OF_PRIME_LINE])
-            three_values.append(value[0])
+            # If first pot line, always prime value
+            if i == 0:
+                value = constants.UNIQUE_LINE_VALUES[line][0]
+                three_values.append(value)
+
+            # If not first pot line, use non-prime value 90% of the time
+            else:
+                value = random.choices(constants.UNIQUE_LINE_VALUES[line], weights=[constants.CHANCE_OF_PRIME_LINE, 1 - constants.CHANCE_OF_PRIME_LINE])
+                three_values.append(value[0])
+        
+        # Not a stat potential line, i.e. we do not care about the value
         else:
             three_values.append(0)
 
     return (*three_values,)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
